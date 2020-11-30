64bfafc9e85ab06fb5eb8d7ea53a5107ed9c8ed1
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-11-30 20:15:37 +0200

    Nested calls

--- js/fibo.js
+++ js/fibo.js
@@ -1,13 +1,17 @@
 // 1 1 2 3 5 8 13 21 34 55
 
-function fibo(n) {
-  console.log(`fibo(${n})`);
+function fibo(n, deep = 0) {
+  /*for (var i = 0; i < deep; i++) {
+    document.write(`&nbsp;&nbsp;&nbsp;`);
+  }
+
+  document.write(`fibo(${n})<br />`);*/
 
   if (n == 1) return 1;
   if (n == 2) return 1;
 
-  return fibo(n-1) + fibo(n-2);
+  return fibo(n-1, deep + 1) + fibo(n-2, deep + 1);
 }
 
-var f = fibo(10);
+var f = fibo(43);
 document.write(f);
